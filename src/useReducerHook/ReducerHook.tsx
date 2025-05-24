a517c1757e9cd764552e13aed4c27e4519779e51
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-24 18:27:44 +0600

    added the reducer hook example

--- src/useReducerHook/ReducerHook.tsx
+++ src/useReducerHook/ReducerHook.tsx
@@ -1,3 +1,21 @@
+import { useReducer } from 'react';
+
 export const ReducerHook = () => {
-  return <div>ReducerHook</div>;
+  const reducer = (state: any, action: any) => {
+    console.log(state, action);
+    if (action.type === 'INCREMENT') return (state = state + 1);
+    if (action.type === 'DECREMENT') {
+      return (state = state - 1);
+    }
+  };
+
+  const [count, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <div className="grid">
+      <h1>{count}</h1>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
+    </div>
+  );
 };
